80a7571e1eeb684abd1e0ac1cf776b56d602e1ea
Mohammad Golam Dostogir <dostogircse171@gmail.com>
2023-12-22 23:10:36 +0000

    Update directory name

--- templatetags/dynamic_agenda_tags.py
+++ templatetags/dynamic_agenda_tags.py
@@ -1,9 +1,9 @@
 from django import template
-from dynamic_agenda.models import AgendaItem
+from django_dynamic_agenda.models import AgendaItem
 
 register = template.Library()
 
-@register.inclusion_tag('dynamic_agenda/agenda_items.html')
+@register.inclusion_tag('django_dynamic_agenda/agenda_items.html')
 def show_agenda():
     items = AgendaItem.objects.all()
     return {'items': items}
