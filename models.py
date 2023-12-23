c653c03ba185201b80acba5ac43d37881133799e
dostogircse171 <dostogircse171@gmail.com>
2023-12-23 00:51:16 +0000

    Add group feature to agenda model

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+class AgendaGroup(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class AgendaItem(models.Model):
+    group = models.ForeignKey(AgendaGroup, on_delete=models.CASCADE, related_name='items')
     time = models.CharField(max_length=100)
     description = models.TextField()
 
