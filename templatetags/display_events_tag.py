13763f3219b1b432909c939a213e89e4b6c717f0
Mohammad Golam Dostogir <dostogircse171@gmail.com>
2023-12-30 15:12:57 +0000

    Update event name to ID with  a message

--- templatetags/display_events_tag.py
+++ templatetags/display_events_tag.py
@@ -13,43 +13,40 @@ def calculate_days_until_event(event):
     return abs(days_until_event) if is_negative else days_until_event, is_negative
 
 @register.inclusion_tag('django_events_timetable/event_items.html')
-def display_event(event_name=None, items_limit=None):
-    days_until_event = None
-    is_negative = False
-    
+def display_event(event_id=None, items_limit=None):
+    """ Display events based on the event ID and items limit, 
+        or a message if the ID is not found. """
+
     try:
-        if items_limit:
-            items_limit = int(items_limit)
+        items_limit = int(items_limit) if items_limit else None
     except ValueError:
         items_limit = None
 
-    if event_name:
+    # Initialize variables
+    days_until_event = None
+    is_negative = False
+    message = None
+
+    if event_id:
         try:
-            event = Event.objects.get(name=event_name)
+            event = Event.objects.get(pk=event_id)
             items = event.items.order_by('start_time')
-            # Calculate days until the event
-            days_until_event = days_to_date(event.start_date)
-            is_negative = days_until_event<0
-            if is_negative:
-                days_until_event = abs(days_until_event)
+            days_until_event, is_negative = calculate_days_until_event(event)
         except Event.DoesNotExist:
-            items = TimeTable.objects.none()
+            items = []
+            message = f"Event ID-{event_id} not found"
     else:
         latest_event = Event.objects.annotate(
             latest_date=Max('items__created')
         ).order_by('-latest_date').first()
 
         if latest_event:
-            items = latest_event.items.all().order_by('start_time')
-            # Calculate days until the event
-            days_until_event = days_to_date(latest_event.start_date)
-            is_negative = days_until_event<0
-            if is_negative:
-                days_until_event = abs(days_until_event)
+            items = latest_event.items.order_by('start_time')
+            days_until_event, is_negative = calculate_days_until_event(latest_event)
         else:
             items = TimeTable.objects.none()
 
     if items_limit:
         items = items[:items_limit]
 
-    return {'items': items, 'days_until_event': days_until_event, "is_negative":is_negative}
+    return {'items': items, 'days_until_event': days_until_event, "is_negative": is_negative, 'message': message}
